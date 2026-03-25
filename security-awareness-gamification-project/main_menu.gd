223093233be37b817af0dd90326e2a5ac80b39de
Sonarc-sys <acer213@gmail.com>
2026-03-25 12:34:34 -0400

    Gave Exit Button Functionality

---
+++ security-awareness-gamification-project/main_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# Main Menu button menus
+func _new_game_pressed() -> void:
+	print("Start Pressed")
+	# get_tree().change_scene_to_file() #link to load a cutscene for new game
+
+
+func _on_continue_pressed() -> void:
+	print("Continue Pressed") 
+	#link to load saved file and new scene
+
+
+func _on_options_pressed() -> void:
+	print("options Pressed") #implement options scene
+
+
+func _on_exit_pressed() -> void:
+	print("Exit Pressed")
+	get_tree().quit() #terminate game
