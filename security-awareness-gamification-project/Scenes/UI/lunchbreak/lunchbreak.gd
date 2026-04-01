35400c3448b2b43449dde938e63354974b88dd40
V Gappasov <vladimir.gappasov18@outlook.com>
2026-04-01 13:18:10 -0400

    Updated lunchbreak

--- security-awareness-gamification-project/Scenes/UI/lunchbreak/lunchbreak.gd
+++ security-awareness-gamification-project/Scenes/UI/lunchbreak/lunchbreak.gd
@@ -1,9 +1,9 @@
 extends Control
-# Remove 'class_name LunchBreakUI' if it's still here
 
-@onready var question_label: Label = $Background/Label # Matches Screenshot 801
-@onready var option1_butt: Button = $Background/Button # Matches Screenshot 801
-@onready var option2_butt: Button = $Background/Button2 # Matches Screenshot 801
+
+@onready var question_label: Label = $Background/Label 
+@onready var option1_butt: Button = $Background/Button 
+@onready var option2_butt: Button = $Background/Button2 
 @onready var consequence_panel: Panel = $ConsequencePanel
 
 var current_scen: LunchBreakScenario
@@ -16,12 +16,11 @@ func display_scenario(scenario: LunchBreakScenario):
 	consequence_panel.hide()
 
 func _process_choice(index: int):
-	# Safety check to make sure a scenario is loaded
+	
 	if not current_scen: return
 	
 	var is_right = (index == current_scen.correct_choiceindex)
 	
-	# Accessing your Global.gd score_Manager
 	if not is_right:
 		if has_node("/root/Global"):
 			get_node("/root/Global").score_Manager.add_penalty(50)
@@ -30,11 +29,8 @@ func _process_choice(index: int):
 
 func show_consequence(right: bool):
 	consequence_panel.show()
-	var result_text = "This is right, nice job! " if right else "Sorry, this is not right. "
-	# Make sure you have a Label named ResultLabel inside your ConsequencePanel
+	var result_text = "This is right, nice job! " if right else "Sorry, this is not right."
 	$ConsequencePanel/ResultLabel.text = result_text + current_scen.consequence_Text
-
-# --- Signal Connections from Screenshots 802 & 803 ---
 
 func _on_button_pressed() -> void:
 	_process_choice(0) # "Yes" / Option 1
