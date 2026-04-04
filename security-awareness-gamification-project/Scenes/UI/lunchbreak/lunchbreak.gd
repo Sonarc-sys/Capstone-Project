fbcc8378dd8314a7253d6d853938af4e12591ce5
V Gappasov <vladimir.gappasov18@outlook.com>
2026-04-04 10:51:24 -0400

    Changed LunchBreak, so that LunchBreak user interface will choose a random scenario from the list of scenarios, and will populate the user interface with that.

--- security-awareness-gamification-project/Scenes/UI/lunchbreak/lunchbreak.gd
+++ security-awareness-gamification-project/Scenes/UI/lunchbreak/lunchbreak.gd
@@ -6,15 +6,29 @@ extends Control
 @onready var option2_butt: Button = $Background/Button2 
 @onready var consequence_panel: Panel = $ConsequencePanel
 
-var current_scen: LunchBreakScenario
+@export var scenario_generator: LunchBreakScenarioGenerator
 
+var current_scen: LunchBreakScenario
+func _ready() -> void:
+	consequence_panel.hide() #Here, ensure that the consequence feedback is hidden until the completion of the lunch scenario.
+	if scenario_generator:
+		#Here, use the custom function to get a scenario from the list of possible scenarios.
+		var random_scen = scenario_generator.get_rand_lunchbreak()
+		if random_scen:
+			display_scenario(random_scen)
+	else:
+		#Here, we have error handling, if I forgot to link the generator, this will prevent the game from randomly crashing with a blank or white screen.
+		print("Sorry, but there is no Scenario Generator that is linked to the LunchBreakUI")
+
+#Here, we will now populate the UI. We will take the data from Resource, and put it on the screen. 
 func display_scenario(scenario: LunchBreakScenario):
 	current_scen = scenario
 	question_label.text = scenario.scenario_text
 	option1_butt.text = scenario.thechoices[0]
 	option2_butt.text = scenario.thechoices[1]
-	consequence_panel.hide()
-
+	
+	
+	#Here, we will check the player's choice against whether the correct answer or not.
 func _process_choice(index: int):
 	
 	if not current_scen: return
