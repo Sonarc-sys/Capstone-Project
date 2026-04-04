4ba9e39c9d68ca52fed5337fb343af653a60e023
Sonarc-sys <acer213@gmail.com>
2026-04-04 14:33:06 -0400

    Left side email display set up with persistance during session

--- security-awareness-gamification-project/Scenes/UI/emailInbox/email_inbox.gd
+++ security-awareness-gamification-project/Scenes/UI/emailInbox/email_inbox.gd
@@ -1,8 +1,5 @@
 extends Control
 
-@export var email_number = 10 #email amount can be changed accordingly
-@export var email_resource_folder = "res://Scripts/Data/emailDatabase/Emails/Day1/" #must make dynamic after demo
-
 @onready var email_list_panel = $"Hbox for EmaiList_Email Body/EmailList Panel"
 
 var all_emails: Array[Email] = [] #array storing all emails from source
@@ -10,45 +7,12 @@ var selected_emails: Array[Email] = [] #10 selected emails stored here
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_all_emails() #loads all emails from the resource folder
-	pick_random_emails() #will then pick 10 random ones
-	email_list_panel.populate_email_list(selected_emails)
-
-#Email functions to load and then pick random emails
-func load_all_emails():
-	all_emails.clear()
-	var dir = DirAccess.open(email_resource_folder)
-	if not dir:
-		push_error("Folder not found")
-		return
+	#Calls email generator to load emails
+	EmailGenerator.load_emails(EmailGenerator.email_resource_folder, EmailGenerator.email_number)
 	
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		if file_name.ends_with(".tres"):
-			var path = email_resource_folder + file_name
-			var email_res = load(path)
-			if email_res is Email:
-				all_emails.append(email_res)
-		file_name = dir.get_next()
-	dir.list_dir_end()
-
-func pick_random_emails():
-	selected_emails.clear()
-	if all_emails.size() <= email_number:
-		selected_emails = all_emails.duplicate()
-		return
-	var available_indices = all_emails.size()
-	var indices = []
-	for i in range(all_emails.size()):
-		indices.append(i)
-	indices.shuffle()  # Randomize indices
-	for i in range(email_number):
-			selected_emails.append(all_emails[indices[i]])
+	#populates left email panel
+	email_list_panel.populate_email_list(EmailGenerator.selected_emails)
 	
-	
-
-
 	
 #Button to bring back to desktop
 func _on_button_pressed() -> void:
---
+++ security-awareness-gamification-project/Scripts/Data/emailDatabase/Logic/EmailGenerator.gd
@@ -0,0 +1,44 @@
+extends Node
+
+@export var email_number = 10 #email amount can be changed accordingly
+@export var email_resource_folder = "res://Scripts/Data/emailDatabase/Emails/Day1/" #must make dynamic after demo
+
+var all_emails: Array[Email] = [] #array storing all emails from source
+var selected_emails: Array[Email] = [] #10 selected emails stored here
+
+func load_emails(email_resource_folder, email_number):
+	if all_emails.size() > 0:
+		# Already loaded, skip
+		return
+	#Loads all emails first
+	#all_emails.clear()
+	var dir = DirAccess.open(email_resource_folder)
+	if not dir:
+		push_error("Folder not found")
+		return
+	
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.ends_with(".tres"):
+			var path = email_resource_folder + file_name
+			var email_res = load(path)
+			if email_res is Email:
+				all_emails.append(email_res)
+		file_name = dir.get_next()
+	dir.list_dir_end()
+	print("loaded all emails")
+#Selects emails
+	selected_emails.clear()
+	if all_emails.size() <= email_number:
+		selected_emails = all_emails.duplicate()
+		return
+	var available_indices = all_emails.size()
+	var indices = []
+	for i in range(all_emails.size()):
+		indices.append(i)
+	indices.shuffle()  # Randomize indices
+	for i in range(email_number):
+			selected_emails.append(all_emails[indices[i]])
+	print("Picked emails")
+	
